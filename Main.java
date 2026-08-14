b522df8ef0f4e4b5fb238cbb0a406f089e503940
Ruchita Chaudhari <ruchitanchaudhari145@gmail.com>
2026-08-14 12:13:24 +0530

    new changes

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
+abstract class Employee{
+    private String name;
+    private int id;
+
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+}
+
+
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World!");
