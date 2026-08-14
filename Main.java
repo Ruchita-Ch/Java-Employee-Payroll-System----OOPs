303f9e645cb5dfe96f6f1c5e9bdfa7bb8a25e025
Ruchita Chaudhari <ruchitanchaudhari145@gmail.com>
2026-08-14 16:26:28 +0530

    new changes

--- Main.java
+++ Main.java
@@ -32,7 +32,32 @@ class fullTimeEmployee extends Employee{
 
            this.monthlySalary = monthlySalary;
       }
+
+      @Override
+      public double calculateSalary(){
+        return monthlySalary;
+      }
+          
 }
+
+class PartTimeEmployee extends Employee {
+     private int hoursWorked;
+     private double hourlyRate;
+
+     public PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate){
+        super(name, id);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+     }
+
+     @Override 
+     public double calculateSalary(){
+        return hoursWorked * hourlyRate;
+     }
+} 
+
+
+
 
 
 public class Main{
