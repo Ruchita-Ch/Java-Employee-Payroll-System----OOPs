c75f53551350aae4c55b5a68e6ae23daa511854e
Ruchita Chaudhari <ruchitanchaudhari145@gmail.com>
2026-09-19 23:49:24 +0530

    new changes

--- Main.java
+++ Main.java
@@ -71,6 +71,28 @@ class PayrollSystem{
     public void addEmployee(Employee employee){
         employeeList.add(employee);
     }
+
+    public void removeEmployee(int id){
+        Employee employeeToRemove = null;
+        
+
+        for(Employee employee : employeeList){
+            if(employee.getId() == id){
+                employeeToRemove = employee;
+                break;
+            } 
+        }
+
+        if(employeeToRemove != null){
+            employeeList.remove(employeeToRemove);
+        }
+    }
+
+    public void displayEmployees(){
+        for(Employee employee: employeeList){
+            System.out.println(employee);
+        }
+    }
 } 
 
 
